ccbcee52af338ed936dbc01b75ec6ce1f90e06a7
김영관 <ggukggukee@gmail.com>
2026-02-13 16:51:51 +0700

    updated better auth settings

--- lib/auth.ts
+++ lib/auth.ts
@@ -12,5 +12,15 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  baseURL: process.env.BETTER_AUTH_URL!,
+  trustedOrigins: [process.env.BETTER_AUTH_URL!],
+  advanced: {
+    crossSubDomainCookies: {
+      enabled: true,
+      domain: process.env.BETTER_AUTH_DOMAIN!,
+    },
+    trustedOrigins: [process.env.BETTER_AUTH_TRUSTED_ORIGIN!],
+    useSecureCookies: process.env.NODE_ENV === "production",
+  },
   plugins: [admin(), nextCookies()],
 });
